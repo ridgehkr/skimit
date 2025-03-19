53de430d1af1df74fbef53414b1f25856efdba9d
calebpierce <caleb@firefli.com>
2025-03-19 10:32:56 -0400

    Compress nested comment spacing

--- components/comments/comment-replies.tsx
+++ components/comments/comment-replies.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client'
 
-import { RedditComment } from "@/types/reddit";
-import { Comment } from "./comment";
+import { RedditComment } from '@/types/reddit'
+import { Comment } from './comment'
 
 interface CommentRepliesProps {
-  replies?: RedditComment[];
+  replies?: RedditComment[]
 }
 
 export function CommentReplies({ replies }: CommentRepliesProps) {
   if (!replies?.length) {
-    return null;
+    return null
   }
 
   return (
-    <div className="space-y-6 mt-6 pl-6">
+    <div className='space-y-4 mt-5 pl-5'>
       {replies.map((reply) => (
         <div key={reply.id}>
           <Comment comment={reply} isTopLevel={false} />
         </div>
       ))}
     </div>
-  );
+  )
 }
